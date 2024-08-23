aa6261251c1a3e88bb3811637c999465213d4df7
Baran <baran.soysuren@gmail.com>
2024-08-23 18:58:30 +0200

    create left and right variables

--- Lesson04_JUnit5WithoutMaven/src/com/junit5/lesson4/ReverseString.java
+++ Lesson04_JUnit5WithoutMaven/src/com/junit5/lesson4/ReverseString.java
@@ -4,6 +4,9 @@ public class ReverseString {
 	
 	public String reverseString(String str) {
 		char[] charArray = str.toCharArray();
+		int left = 0;
+		int right = charArray.length - 1;
+		
 		
 		return new String(charArray);
 	}
