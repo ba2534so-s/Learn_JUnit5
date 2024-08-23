5ecefd307ea4b63264757a32587909962a3fa3ed
Baran <baran.soysuren@gmail.com>
2024-08-23 19:08:52 +0200

    create JUnit5 test file

---
+++ Lesson04_JUnit5WithoutMaven/test/com/junit5/lesson4/ReverseStringTest.java
@@ -0,0 +1,14 @@
+package com.junit5.lesson4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ReverseStringTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
