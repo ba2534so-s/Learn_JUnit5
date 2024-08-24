71ca97566e7c0e9812358cbcb168fb71fdd23f6c
Baran <baran.soysuren@gmail.com>
2024-08-24 22:58:49 +0200

    create test file

---
+++ Lesson06_JUnit5WithMaven/src/test/java/com/junit5/lesson6/TestTrial.java
@@ -0,0 +1,14 @@
+package com.junit5.lesson6;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestTrial {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
