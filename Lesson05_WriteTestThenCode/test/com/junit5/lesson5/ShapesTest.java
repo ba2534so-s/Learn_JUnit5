a198bdec8d31b6ee26d9652bdae82697bb45fce0
Baran <baran.soysuren@gmail.com>
2024-08-24 10:09:46 +0200

    Instantiate Shapes class on class level to use in multiple methods

--- Lesson05_WriteTestThenCode/test/com/junit5/lesson5/ShapesTest.java
+++ Lesson05_WriteTestThenCode/test/com/junit5/lesson5/ShapesTest.java
@@ -5,10 +5,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class ShapesTest {
+	Shapes shape = new Shapes();
 
 	@Test
 	void testCalcSquareArea() {
-		Shapes shape = new Shapes();
 		
 		assertEquals(576, shape.calcSquareArea(24));
 	}
