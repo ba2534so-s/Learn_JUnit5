0ed3388c206217c52999cb15427e9951d30ef6a9
Baran <baran.soysuren@gmail.com>
2024-08-24 09:59:51 +0200

    create test file

---
+++ Lesson05_WriteTestThenCode/test/com/junit5/lesson5/ShapesTest.java
@@ -0,0 +1,14 @@
+package com.junit5.lesson5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ShapesTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
