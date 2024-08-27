0b065aeb3fd7e64baa577913eb2bb6227baf047f
Baran <baran.soysuren@gmail.com>
2024-08-27 22:22:59 +0200

    Create TestingPerformance file

---
+++ Lesson10_TestingPerformance/test/com/junit5/lesson10/TestingPerformance.java
@@ -0,0 +1,14 @@
+package com.junit5.lesson10;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestingPerformance {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
