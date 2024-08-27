bed62f17a57f220d7193165c2d76831c64539103
Baran <baran.soysuren@gmail.com>
2024-08-27 21:48:27 +0200

    create TestException file

---
+++ Lesson09_AssertingWithExceptions/test/com/junit5/lesson9/TestException.java
@@ -0,0 +1,14 @@
+package com.junit5.lesson9;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestException {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
