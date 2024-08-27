f5ddfe9963daf9ab6d397c3d374f12d50f4826eb
Baran <baran.soysuren@gmail.com>
2024-08-27 23:11:17 +0200

    add method algorithm

--- Lesson10_TestingPerformance/src/com/junit5/lesson10/SortArray.java
+++ Lesson10_TestingPerformance/src/com/junit5/lesson10/SortArray.java
@@ -1,8 +1,13 @@
 package com.junit5.lesson10;
 
+import java.util.Arrays;
+
 public class SortArray {
 	
 	public int[] sortArray(int[] array) {
-		
+		for (int i = 0; i < 1000000; i++) {
+			Arrays.sort(array);
+		}
+		return array;
 	}
 }
