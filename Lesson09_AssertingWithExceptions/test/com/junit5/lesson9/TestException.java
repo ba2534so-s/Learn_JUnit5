88c64ea88ee68c5caac878afc7155b449c78353d
Baran <baran.soysuren@gmail.com>
2024-08-27 21:49:35 +0200

    create instance of SortingArray

--- Lesson09_AssertingWithExceptions/test/com/junit5/lesson9/TestException.java
+++ Lesson09_AssertingWithExceptions/test/com/junit5/lesson9/TestException.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class TestException {
+	
+	SortingArray sorter = new SortingArray();
 
 	@Test
 	void testSortingArray_exception() {
