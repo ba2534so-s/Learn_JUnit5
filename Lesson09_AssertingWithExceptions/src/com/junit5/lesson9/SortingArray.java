6785a6b14d6e7a97fd27e13e58f29f3a18192db1
Baran <baran.soysuren@gmail.com>
2024-08-27 21:45:39 +0200

    create sortArray alghoritm

--- Lesson09_AssertingWithExceptions/src/com/junit5/lesson9/SortingArray.java
+++ Lesson09_AssertingWithExceptions/src/com/junit5/lesson9/SortingArray.java
@@ -1,9 +1,12 @@
 package com.junit5.lesson9;
 
+import java.util.Arrays;
+
 public class SortingArray {
 	
 	public int[] sortArray(int[] array) {
-		
+		Arrays.sort(array);
+		return array;
 	}
 
 }
