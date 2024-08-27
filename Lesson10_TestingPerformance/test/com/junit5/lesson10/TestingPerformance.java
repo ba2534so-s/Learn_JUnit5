49f3fc718a03ad9ab44e3861546e42625607a2d6
Baran <baran.soysuren@gmail.com>
2024-08-27 23:21:09 +0200

    call assertTimeout

--- Lesson10_TestingPerformance/test/com/junit5/lesson10/TestingPerformance.java
+++ Lesson10_TestingPerformance/test/com/junit5/lesson10/TestingPerformance.java
@@ -2,6 +2,8 @@ package com.junit5.lesson10;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.Test;
 
 class TestingPerformance {
@@ -11,6 +13,8 @@ class TestingPerformance {
 		SortArray sort = new SortArray();
 		
 		int[] unsorted = {7, 3, 4};
+		
+		assertTimeout(Duration.ofMillis(10), () -> sort.sortArray(unsorted));
 	}
 
 }
