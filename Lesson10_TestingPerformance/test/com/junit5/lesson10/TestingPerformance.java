a0bed7d51136ac8470215a466a5266299c4c36b4
Baran <baran.soysuren@gmail.com>
2024-08-27 23:13:14 +0200

    adjust method name and create instance of SortArray

--- Lesson10_TestingPerformance/test/com/junit5/lesson10/TestingPerformance.java
+++ Lesson10_TestingPerformance/test/com/junit5/lesson10/TestingPerformance.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.Test;
 class TestingPerformance {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testSortingMethod_Performance() {
+		SortArray sort = new SortArray();
 	}
 
 }
