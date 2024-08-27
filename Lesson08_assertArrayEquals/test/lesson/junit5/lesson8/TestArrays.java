2411362720682f1a7e916cf56e85348573b14c1a
Baran <baran.soysuren@gmail.com>
2024-08-27 21:36:00 +0200

    sort actual

--- Lesson08_assertArrayEquals/test/lesson/junit5/lesson8/TestArrays.java
+++ Lesson08_assertArrayEquals/test/lesson/junit5/lesson8/TestArrays.java
@@ -2,14 +2,18 @@ package lesson.junit5.lesson8;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 class TestArrays {
 
 	@Test
 	void testArrays() {
-		int[] expected = {2, 3, 7, 5};
-		int[] actual = {2, 3, 5, 7};
+		int[] expected = {2, 3, 5, 9};
+		int[] actual = {2, 9, 5, 3};
+		
+		Arrays.sort(actual);
 		
 		assertArrayEquals(expected, actual);
 	}
