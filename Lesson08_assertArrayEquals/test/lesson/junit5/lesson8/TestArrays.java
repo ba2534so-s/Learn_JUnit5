abe56af1515bc2009688e70867a2a844d24da181
Baran <baran.soysuren@gmail.com>
2024-08-27 21:27:51 +0200

    create testArrays file

---
+++ Lesson08_assertArrayEquals/test/lesson/junit5/lesson8/TestArrays.java
@@ -0,0 +1,14 @@
+package lesson.junit5.lesson8;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestArrays {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
