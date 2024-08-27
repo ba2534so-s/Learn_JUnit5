d42d5a45a03ab1379a224259bb8159fdc76fb341
Baran <baran.soysuren@gmail.com>
2024-08-27 21:52:27 +0200

    caa sortArray

--- Lesson09_AssertingWithExceptions/test/com/junit5/lesson9/TestException.java
+++ Lesson09_AssertingWithExceptions/test/com/junit5/lesson9/TestException.java
@@ -6,11 +6,12 @@ import org.junit.jupiter.api.Test;
 
 class TestException {
 	
-	SortingArray sorter = new SortingArray();
+	SortingArray sort = new SortingArray();
 
 	@Test
 	void testSortingArray_exception() {
 		int[] unsorted = {5, 2, 3};
+		int[] sorted = sort.sortArray(unsorted);
 		
 		fail("Not yet implemented");
 	}
