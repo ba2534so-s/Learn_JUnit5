449c4e8af75fc2367d9c9c936388efa28a5016c9
Baran <baran.soysuren@gmail.com>
2024-08-27 20:56:32 +0200

    create TestShapes file and create instance of Shapes

---
+++ Lesson07_MoreOnAssertEquals/test/com/junit5/lesson7/TestShapes.java
@@ -0,0 +1,16 @@
+package com.junit5.lesson7;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestShapes {
+	
+	Shapes shape = new Shapes();
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
