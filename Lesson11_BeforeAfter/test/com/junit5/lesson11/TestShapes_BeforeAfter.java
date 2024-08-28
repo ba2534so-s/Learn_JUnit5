8ad62a23d0918bdfd580b7c434fc6f384cdc03b5
Baran <baran.soysuren@gmail.com>
2024-08-28 21:43:41 +0200

    add test file

---
+++ Lesson11_BeforeAfter/test/com/junit5/lesson11/TestShapes_BeforeAfter.java
@@ -0,0 +1,14 @@
+package com.junit5.lesson11;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestShapes_BeforeAfter {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
