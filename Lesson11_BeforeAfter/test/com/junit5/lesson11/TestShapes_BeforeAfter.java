077436b32f97cbc517ebb0db504ad29cc6ad9499
Baran <baran.soysuren@gmail.com>
2024-08-28 22:03:06 +0200

    Add BeforeEach annotation

--- Lesson11_BeforeAfter/test/com/junit5/lesson11/TestShapes_BeforeAfter.java
+++ Lesson11_BeforeAfter/test/com/junit5/lesson11/TestShapes_BeforeAfter.java
@@ -2,14 +2,17 @@ package com.junit5.lesson11;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestShapes_BeforeAfter {
 
 	Shapes shape;
 	
+	@BeforeEach
 	void init() {
 		shape = new Shapes();
+		System.out.println("Before Test");
 	}
 	
 	
